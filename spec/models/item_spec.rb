99626e163b1449d71782fd5eb6f23c92862dbe20
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-04-27 11:27:28 -0600

    Adds default values to items

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,5 +11,31 @@ RSpec.describe Item, type: :model do
     it 'is valid' do
       expect(item).to be_valid
     end
+
+    it 'has a default value for image and retired' do
+      expect(item.retired).to eq(false)
+      expect(item.image).to eq("https://browshot.com/static/images/not-found.png")
+    end
+  end
+
+  context 'with invalid params' do
+    it 'is invalid without a title' do
+      item.title = ""
+      expect(item).to_not be_valid
+    end
+
+    it 'is invalid without a description' do
+      item.description = ""
+      expect(item).to_not be_valid
+    end
+
+    it 'is invalid without a price' do
+      item.price = ""
+      expect(item).to_not be_valid
+      item.price = -10
+      expect(item).to_not be_valid
+      item.price = 0
+      expect(item).to_not be_valid
+    end
   end
 end
