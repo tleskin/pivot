626e5d18f906e566511d9eb42fbad90bab4429d4
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-04-29 16:16:53 -0600

    Adds category functionality to items

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -6,6 +6,7 @@ class Admin::ItemsController < ApplicationController
 
   def new
     @item = Item.new
+    @categories = Category.all
   end
 
   def create
@@ -20,11 +21,14 @@ class Admin::ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    @categories = Category.all
   end
 
   def update
     @item = Item.find(params[:id])
+    categories = params[:item][:category_ids].reject(&:empty?)
     if @item.update(item_params)
+      categories.each { |cat| @item.categories << Category.find(cat)}
       redirect_to admin_items_path
     else
       flash[:notice] = "Invalid fields"
@@ -39,7 +43,7 @@ class Admin::ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :description, :price, :image, :retired)
+    params.require(:item).permit(:title, :description, :price, :image, :retired, :category_ids)
   end
 
 end
