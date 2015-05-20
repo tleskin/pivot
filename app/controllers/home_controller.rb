53193eaf59b853ec6bdf35ca84c8cc089b0932a6
Thomas Leskin <tleskin@gmail.com>
2015-05-20 08:27:52 -0600

    Add homepage

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+
+  def index
+  end
+
+  def not_found
+    flash[:error] = "Page not found"
+    redirect_to root_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root "home#index"
+
   namespace :admin do
     resources :items
     resources :categories
@@ -31,8 +33,7 @@ Rails.application.routes.draw do
   post '/add_item', to: 'carts#add_item'
   post '/subtract_item', to: 'carts#subtract_item'
   post '/remove_item', to: 'carts#remove_item'
-  get '/', to: 'unauth_sessions#show'
-  root 'unauth_sessions#show'
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
