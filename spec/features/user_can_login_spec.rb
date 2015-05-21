5d80e854082c7b6b64222a4a94100db98053e443
Thomas Leskin <tleskin@gmail.com>
2015-05-21 07:44:36 -0600

    Fix user logout test

--- spec/features/user_can_login_spec.rb
+++ spec/features/user_can_login_spec.rb
@@ -3,15 +3,11 @@ require 'rails_helper'
 RSpec.describe 'user' do
   context 'with an active account' do
     it 'can login' do
-      User.create(first_name: "Steve",
-                  last_name: "Jones",
-                  username: "sjones",
-                  email: "s@jones.com",
-                  password: "password",
-                  password_confirmation: "password")
+      create(:user)
+
       visit root_path
       click_link "Login"
-      fill_in "session[email]", with: "s@jones.com"
+      fill_in "session[email]", with: "user@example.com"
       fill_in "session[password]", with: "password"
       click_button "Login"
       expect(page).to have_content("Steve")
