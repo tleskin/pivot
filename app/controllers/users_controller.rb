bc3c750f210c533b69c8f60c775e568fecda970b
Thomas Leskin <tleskin@gmail.com>
2015-05-27 18:58:00 -0600

    Integrated email with SendGrid, signup email working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserNotifier.send_signup_email(@user).deliver_now
       session[:user_id] = @user.id
       redirect_to @user
     else
