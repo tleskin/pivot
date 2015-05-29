490b07c50ac00bdd0f85f084a5409e8f2f4a5328
Thomas Leskin <tleskin@gmail.com>
2015-05-29 02:03:09 -0600

    Remove stuff from environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.active_record.raise_in_transactional_callbacks = true
-config.exceptions_app = routes
-config.action_mailer.perform_deliveries = true
 config.action_mailer.delivery_method = :smtp
 
 ActionMailer::Base.smtp_settings = {
