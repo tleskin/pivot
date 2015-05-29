2f40611b2356d381f0c4475e42c19ca04bd41ccd
Thomas Leskin <tleskin@gmail.com>
2015-05-29 01:58:14 -0600

    Update mailer for production

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module DinnerDash
     ActionMailer::Base.smtp_settings = {
       user_name: ENV["SENDGRID_USERNAME"],
       password: ENV["SENDGRID_PASSWORD"],
-      domain: "lendio.social",
+      domain: "lendio.herokuapp.com",
       address: "smtp.sendgrid.net",
       port: 587,
       authentication: :plain,
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.active_record.raise_in_transactional_callbacks = true
+config.exceptions_app = routes
+config.action_mailer.perform_deliveries = true
+config.action_mailer.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV["SENDGRID_USERNAME"],
+  password: ENV["SENDGRID_PASSWORD"],
+  domain: "lendio.herokuapp.com",
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
