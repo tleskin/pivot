ded41f59bc0e71b0093a3da5e5608cf2d31b5115
Thomas Leskin <tleskin@gmail.com>
2015-05-29 02:04:25 -0600

    Remove stuff from environment

--- config/environment.rb
+++ config/environment.rb
@@ -4,8 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-config.action_mailer.delivery_method = :smtp
-
 ActionMailer::Base.smtp_settings = {
   user_name: ENV["SENDGRID_USERNAME"],
   password: ENV["SENDGRID_PASSWORD"],
